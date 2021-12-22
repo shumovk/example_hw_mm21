05c568f4fca3c0d8b7d33ea2b9687a1f52ef3c31
shumovk <90559738+shumovk@users.noreply.github.com>
2021-12-22 21:04:02 +0300

    Update hw1.cpp

--- homework1/hw1.cpp
+++ homework1/hw1.cpp
@@ -1,14 +1,51 @@
+#include <stdlib.h>
 #include <iostream>
 #include <fstream>
+#include <cmath>
+
+bool is_on_left(int x0, int y0, int x, int y) {
+	if (y0*x < x0*y) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+double scalar_prod_normalized(int x0, int y0, int x, int y) {
+	return (x0*x+y0*y)/(sqrt(x*x+y*y)*sqrt(x0*x0+y0*y0));
+}
 
 int main() {
-    std::ifstream input_file("in.txt");
-    int a;
+	std::ifstream infile("in.txt");
 
-    std::ofstream out_file("result.txt");
-    out_file << "Result";
-    out_file << a*a << '\n';
+	int x, y;
 
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+	int left_x = 0, left_y = 0;
+	int right_x = 0, right_y = 0;
+	double right_cos = 1;
+	double left_cos = 1;
+
+	int x0, y0;
+	infile >> x0 >> y0;
+
+
+	while (infile >> x >> y) {
+
+		double sc_prod = scalar_prod_normalized(x0, y0, x, y);
+		sc_prod = std::round(sc_prod * 10000000000.0) / 10000000000.0;
+		bool on_left = is_on_left(x0, y0, x, y);
+
+		if (sc_prod <= left_cos && on_left) {
+			left_cos = sc_prod;
+			left_x = x; left_y = y;
+
+		}
+		if (sc_prod <= right_cos && !on_left) {
+			right_cos = sc_prod;
+			right_x = x; right_y = y;
+		}
+	}
+
+	std::cout << "Leftmost: " << left_x << " " << left_y << std::endl;
+	std::cout << "Rightmost: " << right_x << " " << right_y << std::endl;
 }
